e34d7c474445705e302fb24cbabd4b2b8bb067d5
H123 <cna.farokhi@gmail.com>
2026-02-19 17:04:44 +0330

    Update jwt.d.ts

--- apps/backend/src/jwt.d.ts
+++ apps/backend/src/jwt.d.ts
@@ -1,10 +1,8 @@
 import '@fastify/jwt'
 
-// Make `request.user` strongly typed after `request.jwtVerify()`.
-// Without this, `request.user` defaults to: string | object | Buffer.
 declare module '@fastify/jwt' {
   interface FastifyJWT {
-    payload: { sub: string; username: string }
+    payload: { id: string; username: string }
     user: { id: string; username: string }
   }
 }
